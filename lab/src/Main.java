06bd17b258ee023a973a9e95ee9238555bf4f90e
VVitsiak <vitsiakv@gmail.com>
2024-02-28 19:19:32 +0200

    solution for lab1

---
+++ lab/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * This is my first lab1 program.
+ * @author VVitsiak
+ * @version 1.0
+ * @since 2024-23-02
+ */
+public class Main {
+    /**The HelloWorld program implements an application that
+     * simply displays "Hello World!" to the standard output.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
