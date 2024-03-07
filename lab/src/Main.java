acd7129ac5c093034bf77af89c62ab6158a511b1
VVitsiak <vitsiakv@gmail.com>
2024-03-07 18:44:02 +0200

    lab2

--- lab/src/Main.java
+++ lab/src/Main.java
@@ -1,15 +1,39 @@
-/**
- * This is my first lab1 program.
- * @author VVitsiak
- * @version 1.0
- * @since 2024-23-02
- */
 public class Main {
-    /**The HelloWorld program implements an application that
-     * simply displays "Hello World!" to the standard output.
-     */
-    public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Введіть радіус круга як аргумент командного рядка");
+        }
+
+        double radius;
+        try {
+            radius = Double.parseDouble(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Спробуйте числове значення!");
+            throw e;
+        }
+        double circleArea = calculateCircleArea(radius);
+        double rectangleArea = calculateRectangleAreaInCircle(radius);
+
+        System.out.println("Площа круга: " + circleArea);
+        System.out.println("Площа вписаного прямокутника: " + rectangleArea);
+    }
+
+    /**
+     * @param radius od the circule to caclulate the area
+     * @return the area of the circle
+     */
+    private static double calculateCircleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    /**
+     * @param radius
+     * @return the area of the rectangle
+     */
+    private static double calculateRectangleAreaInCircle(double radius) {
+        double sideLength = 2 * radius / Math.sqrt(2);
+        return sideLength * sideLength;
+
     }
 }
